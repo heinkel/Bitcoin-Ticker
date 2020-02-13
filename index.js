d3ada81dac287f984903df562f1a5e4b1083dd1a
JAO Heinkel <juan.andres.otz@gmail.com>
2020-02-14 02:28:19 +0900

    different currencies consultation added

--- index.js
+++ index.js
@@ -10,15 +10,33 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"));
 
 app.post("/", (req,res)=> {
-    // console.log(req.body.crypto)
+    var crypto_value = req.body.crypto;
+    var fiat_value = req.body.fiat;
+    let data = 0;
+    let value = 0;
+    let price = "price_" +fiat_value.toLowerCase();
+   
+   //original URL https://api.alternative.me/v1/ticker/bitcoin/?convert=USD
+    var DinamiclURL = "https://api.alternative.me/v1/ticker/"+crypto_value+"/?convert="+fiat_value;
+
+    request(DinamiclURL, (error, response, body) => {
+   
+        data = JSON.parse(body);
+       
+      
+        value = Number(data[0][`${price}`]).toFixed(2);
+        console.log(value);
+        let btcValue = Number(data[0].price_btc).toFixed(4);
+        
+        res.write(`<h1>The Price of ${crypto_value} Today is: ${value} ${fiat_value}.</h1>`);
+        res.write(`<p>The Price in btc  for every ${crypto_value}  is: ${btcValue} bitcoins.</p>`);
+
+        res.send();  //this send to browser what have been written using res.write() allowing multiple information comming from request
+
+
+    });
     
-request('https://api.alternative.me/v1/ticker/bitcoin/?convert=USD', (error, response, body) => {
-    console.log(response.statusCode);  // IF returns 200 everything is OK.
-//   console.error('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-
-});
 });
 
-app.listen(port, () => console.log("Server is running on port 3000."));
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
+
